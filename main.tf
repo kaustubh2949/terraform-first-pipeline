5ec7d7002767bf299de06b28dd14a09e3de91d46
kaustubh2949 <kaustubhtandale1058gmail.com>
2025-07-30 10:01:08 +0530

    changes done in main.tf added security group

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  security_groups = ["sg-035dc95b00e35817c"]
+
 
   tags = {
     Name = var.instance_name
